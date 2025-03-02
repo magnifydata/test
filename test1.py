9d8d49258f61f766877b98b603d694b57c72e7dc
magnifydata <shamindavictorysg@gmail.com>
2025-03-02 22:50:44 +0800

    test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.write('Hello World')
+# Set the title of the app
+st.title("Welcome to My Streamlit App")
+
+# Display some styled text
+st.markdown("""
+    <style>
+    .big-font {
+        font-size: 30px;
+        color: #4CAF50;
+        font-weight: bold;
+    }
+    </style>
+    <p class="big-font">Hello, this is my first Streamlit app with custom styling!</p>
+""", unsafe_allow_html=True)
+
+# Add some additional content
+st.write("Streamlit is an amazing framework for building web apps in Python!")
+st.write("You can add widgets, charts, and much more with just a few lines of code.")
